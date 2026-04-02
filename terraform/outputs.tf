2ad05cad1d2279ad6ea0c4a598da76e50176b90f
Cielo Escobar <ccieloescobar@gmail.com>
2026-04-02 11:22:26 +0200

    Expose networking outputs in root module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,32 @@ output "common_name_prefix" {
   description = "Common prefix used for resource naming"
   value       = local.common_name_prefix
 }
+output "vpc_id" {
+  description = "ID of the main VPC"
+  value       = module.networking.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.networking.public_subnet_ids
+}
+
+output "private_app_subnet_ids" {
+  description = "IDs of the private application subnets"
+  value       = module.networking.private_app_subnet_ids
+}
+
+output "private_db_subnet_ids" {
+  description = "IDs of the private database subnets"
+  value       = module.networking.private_db_subnet_ids
+}
+
+output "internet_gateway_id" {
+  description = "ID of the internet gateway"
+  value       = module.networking.internet_gateway_id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT gateway"
+  value       = module.networking.nat_gateway_id
+}
