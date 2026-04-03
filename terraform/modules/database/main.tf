e14194bda1575c56036dcb5bd493b1442ef7a939
Cielo Escobar <ccieloescobar@gmail.com>
2026-04-03 03:21:08 +0200

    Fix terraform formatting

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -8,19 +8,19 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_db_instance" "this" {
-  identifier              = "nimbus-dev-db"
-  engine                  = "mysql"
-  engine_version          = "8.0"
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
-  db_name                 = "nimbus"
-  username                = "admin"
-  password                = "Password123!"
-  db_subnet_group_name    = aws_db_subnet_group.this.name
-  vpc_security_group_ids  = [var.db_security_group_id]
-  skip_final_snapshot     = true
-  publicly_accessible     = false
-  multi_az                = false
+  identifier             = "nimbus-dev-db"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  db_name                = "nimbus"
+  username               = "admin"
+  password               = "Password123!"
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [var.db_security_group_id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  multi_az               = false
 
   tags = {
     Name = "nimbus-dev-db"
